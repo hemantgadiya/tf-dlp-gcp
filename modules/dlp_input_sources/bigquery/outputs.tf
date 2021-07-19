afb677b77549a5745e048d9640872e45a32fb4a6
hemantgadiya <57321739+hemantgadiya@users.noreply.github.com>
2021-07-19 11:49:50 +0530

    Update outputs.tf
    Updated

--- modules/dlp_input_sources/bigquery/outputs.tf
+++ modules/dlp_input_sources/bigquery/outputs.tf
@@ -2,6 +2,6 @@ output "bq_dataset_id" {
   value = "${google_bigquery_dataset.source_dataset.id}"
 }
 
-output "bg_table_id" {
+output "bq_table_id" {
   value = "${google_bigquery_table.default.id}"
 }
