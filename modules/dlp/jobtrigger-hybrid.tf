129f8f139afca77ee0ca9530d13a12b4338748af
hemantgadiya <57321739+hemantgadiya@users.noreply.github.com>
2021-08-08 14:56:28 +0530

    Update jobtrigger-hybrid.tf

--- modules/dlp/jobtrigger-hybrid.tf
+++ modules/dlp/jobtrigger-hybrid.tf
@@ -1,1 +1,24 @@
 
+resource "google_data_loss_prevention_job_trigger" "basic-hybrid-job" {
+    parent          = "projects/${var.project}"
+    description     = "Reoccuring search on hybrid datasource"
+    display_name    = "Sample Hybrid Job"
+
+    inspect_job {
+        inspect_template_name = google_data_loss_prevention_inspect_template.basic.id
+        actions {
+            save_findings {
+                output_config {
+                    table {
+                        project_id = var.project
+                        dataset_id = split("/", var.bq_output_dataset_id)[3]
+                    }
+                }
+            }
+        }
+        storage_config {
+            hybrid_options {
+            }
+        }
+    }
+}
